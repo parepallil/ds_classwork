788d298614b9e5d55315c0b58edb36e485d9b2e0
parepallil <manoharparepallil2001@gmail.com>
2024-03-04 15:17:12 -0500

    My Second commit

--- classsWork-7.cpp
+++ classsWork-7.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <string>
 
+// Define a class named BasketballMatch to represent a basketball match
 class BasketballMatch {
 private:
+    // Private member variables for team names and scores
     std::string team1;
     std::string team2;
     int scoreTeam1;
     int scoreTeam2;
 
 public:
-    // Constructor
+    // Constructor for BasketballMatch class
     BasketballMatch(const std::string& t1, const std::string& t2, int s1, int s2)
         : team1(t1), team2(t2), scoreTeam1(s1), scoreTeam2(s2) {}
 
@@ -63,18 +65,19 @@ public:
     }
 };
 
+// Main function where the program execution starts
 int main() {
-    // Example usage:
+    // Create an instance of the BasketballMatch class
     BasketballMatch match("TeamA", "TeamB", 78, 82);
 
-    // Using getter methods
+    // Using getter methods to display initial information
     std::cout << "Team 1: " << match.getTeam1() << std::endl;
     std::cout << "Team 2: " << match.getTeam2() << std::endl;
     std::cout << "Score Team 1: " << match.getScoreTeam1() << std::endl;
     std::cout << "Score Team 2: " << match.getScoreTeam2() << std::endl;
     std::cout << "Winner: " << match.determineWinner() << std::endl;
 
-    // Using setter methods
+    // Using setter methods to modify the match information
     match.setTeam1("Lakers");
     match.setTeam2("Warriors");
     match.setScoreTeam1(92);
@@ -88,5 +91,6 @@ int main() {
     std::cout << "Score Team 2: " << match.getScoreTeam2() << std::endl;
     std::cout << "Winner: " << match.determineWinner() << std::endl;
 
+    // Return 0 to indicate successful program execution
     return 0;
 }
